e6ebd87c7093dbd25f49f2862989cff21c53fe82
ivan-skukan <is53961@fer.hr>
2023-11-07 16:49:19 +0100

    dodana skripta za glavnu stranicu

---
+++ izvorniKod/public/scripts/main.js
@@ -0,0 +1,46 @@
+let rezZanr = 0;
+let rezTop = 0;
+let btnZarn = document.getElementById('zarn');
+let mapa = document.getElementById('mapa');
+let listaZ = document.getElementById('listaZanr');
+let listaT = document.getElementById('listaTop');
+
+
+let showZanr = function() {
+
+    if (rezZanr == 0) {
+    
+        mapa.style.display = "none";
+        listaZ.style.display = "flex"
+        listaT.style.display = "none"
+        rezZanr = 1;
+        rezTop = 0;
+
+    } else if (rezZanr == 1) {
+    
+        mapa.style.display = "flex";
+        listaZ.style.display = "none";
+        listaT.style.display = "none"
+        rezZanr = 0;
+        rezTop = 0;
+    }
+};
+let showTop = function() {
+
+    if (rezTop == 0) {
+    
+        mapa.style.display = "none";
+        listaZ.style.display = "none"
+        listaT.style.display = "flex"
+        rezZanr = 0;
+        rezTop = 1;
+
+    } else if (rezTop == 1) {
+    
+        mapa.style.display = "flex";
+        listaZ.style.display = "none";
+        listaT.style.display = "none"
+        rezZanr = 0;
+        rezTop = 0;
+    }
+};
